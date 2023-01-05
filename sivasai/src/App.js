d00c3e892b6660b2b7787890307b0385b311e181
sivasai <sivasai3920@gmail.com>
2023-01-05 15:28:23 +0530

    Class based component created

--- sivasai/src/App.js
+++ sivasai/src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Welcome } from "./component/Sivasai";
-
+import Jan5 from "./component/Jan5";
 function App() {
   return (
     <div className="App">
       <Welcome />
+      <Jan5 />
     </div>
   );
 }
---
+++ sivasai/src/component/Jan5.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+// Class based component
+class Jan5 extends Component {
+  render() {
+    return <h1>Class based Component</h1>;
+  }
+}
+
+export default Jan5;
