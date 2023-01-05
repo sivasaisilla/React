bc6fe5263e0232217ecd852bd4fadc28dc7b598f
sivasai <sivasai3920@gmail.com>
2023-01-05 16:30:12 +0530

    first component from scratch

--- silla/src/index.js
+++ silla/src/index.js
@@ -1,2 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+
+function Ineuron() {
+  return <h1>Ineuron Bengaluru</h1>;
+}
+
+ReactDOM.render(<Ineuron />, document.getElementById("root"));
