08e199f13fae2fc874a971f7106abc99abe28f55
sivasai <sivasai3920@gmail.com>
2023-01-06 10:40:08 +0530

    linking css with component file

--- silla/src/index.js
+++ silla/src/index.js
@@ -1,8 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
 function Ineuron() {
-  return <h1>Ineuron Bengaluru</h1>;
+  return (
+    <div className="siva">
+      <Blockchain />
+      <Blockchain />
+      <Blockchain />
+      <Blockchain />
+    </div>
+  );
 }
+
+const Blockchain = () => {
+  return <h4>Blockchain + React is Deadly</h4>;
+};
 
 ReactDOM.render(<Ineuron />, document.getElementById("root"));
